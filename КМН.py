dca25547a5a6409f706d0498729277728e3c2096
GloryFawn <studentitstep03@gmail.com>
2023-10-07 13:53:55 +0300

    КМП

--- КМН.py
+++ КМН.py
@@ -1,46 +1,44 @@
 import random
 
-
-start = input('Вы запустили игру "Камень, ножницы, бумага". Хотите поиграть? (Вводите + или -): ')
+start = input('Ви запустили гру "Камінь, ножиці, папір". Бажаєте пограти? (Вводьте + або -):')
 
 if start == '+':
-    print('Загрузка...')
-    print("Загрузка завершена! Начинаем!")
+    print('Завантаження...')
+    print("Завантаження завершено! Починаємо!")
     print("3...2...1...")
-    print('Если захотите закончить вводите "-".')
-    print('Если захотите узнать счёт вводите "с".')
+    print('Якщо захочете закінчити введіть "-".')
+    print('Якщо захочете дізнатися рахунок вводите "р".')
     user_ball = 0
     rand_ball = 0
     while True:
-        user = input("Камень, ножницы или бумага? (Вводите к, н или б): ")
-        list_play = ['к', 'н', 'б']
+        user = input("Камінь, ножиці або папір? (Вводьте до, н або п): ")
+        list_play = ['к', 'н', 'п']
         if user in list_play:
             rand = random.choice(list_play)
             print(rand)
 
             if rand == 'к' and user == 'н':
                 rand_ball += 1
-            if rand == 'к' and user == 'б':
+            if rand == 'к' and user == 'п':
                 user_ball += 1
             if rand == 'н' and user == 'к':
                 user_ball += 1
-            if rand == 'н' and user == 'б':
+            if rand == 'н' and user == 'п':
                 rand_ball += 1
-            if rand == 'б' and user == 'н':
+            if rand == 'п' and user == 'н':
                 user_ball += 1
-            if rand == 'б' and user == 'к':
+            if rand == 'п' and user == 'к':
                 rand_ball += 1
-        elif user == 'с':
-            print('Ваши баллы - ', user_ball, '. Баллы вашего соперника - ', rand_ball, ".")
+        elif user == 'р':
+            print('Ваши бали - ', user_ball, '. Бали вашого суперника - ', rand_ball, ".")
         elif user == '-':
-            print('Ваши баллы - ', user_ball, '. Баллы вашего соперника - ', rand_ball, ".")
-            print('Конец игры! Заходите ещё!')
+            print('Ваши бали - ', user_ball, '. Бали вашого суперника - ', rand_ball, ".")
+            print('Кінець гри! Заходьте ще!')
             break
         else:
-            print('Вводите к, н или б')
+            print('Введіть к, н или п')
 
-
-if start == '-':
-    print('Жаль... :(')
-else:
-    print('Простите, я вас не понял, если хотите играть перезапустите программу и введите "+". Спасибо!')
+    if start == '-':
+        print('Шкода... :(')
+    else:
+        print('Вибачте, я вас не зрозумів, якщо хочете грати, перезапустіть програму і введіть "+". Дякую!')
